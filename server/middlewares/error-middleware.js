4e0ea12cf77cd00565d0baa95e3d7fb66318c9cc
Oleg Akimov <akimba@gmail.com>
2023-10-12 20:27:01 +0300

    fix: Исправляет структуру ошибки 500

--- server/middlewares/error-middleware.js
+++ server/middlewares/error-middleware.js
@@ -5,7 +5,7 @@ const errorMiddleware = (err, _req, res, _next) => {
     res.status(err.status).json({message: err.message, errors: err.errors});
     return;
   }
-  res.status(500).json(err.message);
+  res.status(500).json({message: err.message});
 };
 
 module.exports = errorMiddleware;
